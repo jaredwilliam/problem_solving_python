be27a9f57acbbbaab683738665c28fef0a6baf38
Jared Early <46874230+jaredwilliam@users.noreply.github.com>
2025-03-22 07:48:30 -0400

    added simulation functions

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import random
 
 
-def generate_guess(pool: list, length: int) -> str:
-    generation = random.choices(pool, k=length)
-    return generation
-    # result = "".join(generation)
-    # return result
+def generate_guess(pool: list, length: int) -> list:
+    guess = random.choices(pool, k=length)
+    return guess
 
 
-def check_guess(guess: list, answer: list):
+def simulate_guesses(pool: list, length: int, num_sims: int = 1) -> list:
+    guesses = []
+    for i in range(num_sims):
+        guesses.append(generate_guess(pool, length))
+    return guesses
+
+
+def score_guess(guess: list, answer: list) -> float:
     length = len(answer)
     correct = 0
-    for g_i, t_i in enumerate(zip(guess, answer)):
+    for pair in list(zip(guess, answer)):
+        if pair[0] == pair[1]:
+            correct += 1
+    return correct / length
 
-        pass
+
+def score_simulation(guesses: list, answer: list) -> float:
+    # average percent of correct characters
+    pct_correct_per_sim = []
+    for guess in guesses:
+        pct_correct_per_sim.append(score_guess(guess, answer))
+    mean = sum(pct_correct_per_sim) / len(guesses)
+    return mean
 
 
 if __name__ == "__main__":
-
-    target = "methinks it is like a weasel"
 
     pool = [
         " ",
@@ -50,20 +63,36 @@ if __name__ == "__main__":
         "z",
     ]
 
+    target = "methinks it is like a weasel"
+
     target_len = len(target)
 
-    guess = generate_guess(pool=pool, length=target_len)
+    guesses = simulate_guesses(pool=pool, length=target_len, num_sims=1000)
+    print(score_simulation(guesses, target))
 
-    # print(guess)
-    # if guess == target:
-    #     print(":)")
-    # else:
-    #     print(":(")
+    # guess_limit = 10000
 
-    target_as_list = [ch for ch in target]
-    zipped = list(zip(guess, target_as_list))
+    # best_guess = None
+    # best_score = 0
 
-    for item in zipped:
-        print(item[0])
+    # for i in range(guess_limit):
+    #     guess = generate_guess(pool=pool, length=target_len)
+    #     score = score_guess(guess, target)
+
+    #     if score > best_score:
+    #         best_guess = guess
+    #         best_score = score
+    # print(list(zip(best_guess, target)))
+    # print(best_score)
+
+    # target_as_list = [ch for ch in target]
+    # zipped = list(zip(guess, target_as_list))
+
+    # for i, pair in enumerate(zipped):
+    #     if pair[0] == pair[1]:
+    #         print(":)")
+    #     else:
+    #         print(":(")
+
     # for g_i, t_i in enumerate(zip(guess, target_as_list)):
     #     print(g_i, t_i)
